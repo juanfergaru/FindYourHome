b24bbe8090097a5c69cdd529525c329eb951e187
juanfergaru <juanfergaru182@gmail.com>
2023-09-02 20:46:54 -0500

    Primera migracion y update a la BD (creacion de la tabla Countries) desde la entidad Country

--- FindYourHome/FindYourHome.API/Program.cs
+++ FindYourHome/FindYourHome.API/Program.cs
@@ -1,3 +1,6 @@
+using FindYourHome.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
 
