3956241c131358d901d058e289d375ccc34ebc03
juanfergaru <juanfergaru182@gmail.com>
2023-10-13 00:07:30 -0500

    Version estable (resta validar desplegable en el formulario de contract payment y tenant )

---
+++ FindYourHome/FindYourHome.Shared/Entities/Tenant.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace FindYourHome.Shared.Entities
+{
+    public class Tenant//Arrendatario
+    {
+
+        public int Id { get; set; }
+
+        [Display(Name = "Documento")]
+        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Document { get; set; } = null!;
+
+        [Display(Name = "Nombres")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string FirstName { get; set; } = null!;
+
+        [Display(Name = "Apellidos")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string LastName { get; set; } = null!;
+
+        [Display(Name = "Dirección")]
+        [MaxLength(200, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Address { get; set; } = null!;
+
+        [Display(Name = "Foto")]
+        public string Photo { get; set; }
+
+    }
+}
